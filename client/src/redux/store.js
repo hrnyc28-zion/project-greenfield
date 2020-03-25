3e01c047cad42910ed04f447daff0492eac3f968
h01m3s <lin426797@gmail.com>
2020-03-25 11:35:02 -0400

    Use middle with dev tools

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/';
 
-let store = createStore(rootReducer, applyMiddleware(thunk));
+const middleware = [thunk];
+
+let store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
