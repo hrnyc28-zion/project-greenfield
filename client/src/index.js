73ec141943927bc636cb9adbd73fa06121807106
h01m3s <lin426797@gmail.com>
2020-03-25 10:58:20 -0400

    Setup redux folder structure

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
+    {/* use AppContainer in futre */}
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ client/src/redux/actions/currentProduct.js
@@ -0,0 +1,6 @@
+var currentProduct = (product) => ({
+  type: `CURRENT_PRODUCT`,
+  payload: product,
+});
+
+export default currentProduct;
