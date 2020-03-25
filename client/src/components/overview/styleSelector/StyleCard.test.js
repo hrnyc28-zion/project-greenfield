982a0664f51eefe91dd06cd027b2265e787d8ef5
Joe Yan <chengmu.yan@gmail.com>
2020-03-25 19:10:29 -0400

    Add StyleSelector component and test

--- client/src/components/overview/styleSelector/StyleCard.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import StyleCard from './StyleCard';
-
-it('renders the StyleCard', () => {
-  const { getByTestId } = render(<StyleCard />);
-  const component = getByTestId('styleCard');
-  expect(component).toBeInTheDocument();
-});
