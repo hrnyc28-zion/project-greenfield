ae9988ef3fb542c44c68ec2dfa5c27de216164f6
Joe Yan <chengmu.yan@gmail.com>
2020-03-25 19:40:32 -0400

    Fix CartForm test name

--- client/src/components/overview/cartForm/index.test.js
+++ client/src/components/overview/cartForm/index.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import CartForm from './index';
 
-it('renders learn react link', () => {
+it('renders the CartForm', () => {
   const { getByTestId } = render(<CartForm />);
   const component = getByTestId('cartForm');
   expect(component).toBeInTheDocument();
