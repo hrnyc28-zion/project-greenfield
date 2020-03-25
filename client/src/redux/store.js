68b3ff0c575dbf5f81d40715162bcbf576ccf149
h01m3s <lin426797@gmail.com>
2020-03-25 11:00:58 -0400

    Correct rootReducer path

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from './../reducers/main.js';
+import rootReducer from './reducers/';
 
 let store = createStore(rootReducer, applyMiddleware(thunk));
 
