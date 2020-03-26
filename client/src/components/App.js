66f07064b4d47b5abc90d6f7aa52b41d32aa8e84
Dan Silberger <dan.silberger@gmail.com>
2020-03-26 17:42:08 -0400

    Rating Stars component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import RatingStars from './RatingStars.js';
 import '../styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <RatingStars rating="3.5" />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
