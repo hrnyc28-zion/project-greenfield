3d5ed2a03577a04ae8bbf29912e5adecea7107df
Vincent Chen <vc88086@gmail.com>
2020-03-26 03:58:12 -0700

    stubbed Comparison Modal

--- client/src/components/related-items/ComparisonModal.js
+++ client/src/components/related-items/ComparisonModal.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Features from './Features';
 
 const ComparisonModal = () => {
   return (
     <div>
-      <p>hi</p>
+      <p>Comparing</p>
+      <table>
+        <tr>
+          <th>current product</th>
+          <th>features</th>
+          <th>compared product</th>
+        </tr>
+        <Features />
+      </table>
     </div>
   );
 };
---
+++ client/src/components/related-items/Features.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Features = () => {
+  return (
+    <tr>
+      <td>current product</td>
+      <td>features</td>
+      <td>compared product</td>
+    </tr>
+  );
+};
+
+export default Features;
