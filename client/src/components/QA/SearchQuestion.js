1165bff733eac270a09d48e0500fe2a848c677ef
h01m3s <lin426797@gmail.com>
2020-03-26 15:53:57 -0400

    Add testid

--- client/src/components/QA/SearchQuestion.js
+++ client/src/components/QA/SearchQuestion.js
@@ -3,7 +3,11 @@ import React from 'react';
 export const SearchQuestion = () => {
   return (
     <div>
-      <input type="text" placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..." />
+      <input
+        data-testid="searchQuestion"
+        type="text"
+        placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..."
+      />
     </div>
   );
 };
