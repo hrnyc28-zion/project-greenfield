869924cb3d0f623a7cd823172f1cf4596030bcfd
Joe Yan <chengmu.yan@gmail.com>
2020-03-26 16:59:43 -0400

    Update ProductRating and SocialCard components

--- client/src/components/overview/productInfo/ProductRating.js
+++ client/src/components/overview/productInfo/ProductRating.js
@@ -1,9 +1,10 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/accessible-emoji */ // TODO: remove this hint
+/* eslint-disable jsx-a11y/anchor-is-valid */ // TODO: remove this hint
 import React from 'react';
 
 const ProductRating = () => (
   <div data-testid="productRating">
-    ***** <a href="">Read all reviews</a>
+    ⭐⭐⭐⭐⭐ <a href="">Read all reviews</a>
   </div>
 );
 
