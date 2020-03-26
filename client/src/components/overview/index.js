0ab72f5e27c851afaf3fd80f35995a65dba9c3e9
Joe Yan <chengmu.yan@gmail.com>
2020-03-26 17:46:40 -0400

    Render ImageGallery into Overview

--- client/src/components/overview/index.js
+++ client/src/components/overview/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ProductInfo from './productInfo';
+import ImageGallery from './imageGallery';
 
 // const { selected, currentProduct, styles } = sampleStore;
 
 const Overview = ({ sampleStore: { selected, currentProduct, styles } }) => (
   <div data-testid="productOverview">
+    <ImageGallery selected={selected} />
     <ProductInfo
       selected={selected}
       currentProduct={currentProduct}
