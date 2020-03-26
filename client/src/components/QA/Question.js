2353d4b3695b64920a8d6be2e4ac1690bfcc04ef
h01m3s <lin426797@gmail.com>
2020-03-25 20:18:36 -0400

    Create Answers component

--- client/src/components/QA/Question.js
+++ client/src/components/QA/Question.js
@@ -23,19 +23,39 @@ export const Question = ({ currentQuestion }) => {
         </a>
       </div>
       <span style={{ fontSize: 20, fontWeight: 'bold' }}>A: </span>
-      {answers['125161'].body}
-      <div>
-        by {answers['125161']['answerer_name']}
-        {', '}
-        {new Date(answers['125161'].date).formatted()} | Helpful?{' '}
-        <a href="#" style={{ color: '#919191' }}>
-          Yes
-        </a>
-        ({answers['125161'].helpfulness}) |{' '}
-        <a href="#" style={{ color: '#919191' }}>
-          report
-        </a>
-      </div>
+      <Answers answers={answers} />
+    </div>
+  );
+};
+
+// TODO:
+// Put answer to another file
+
+const Answers = ({ answers }) => {
+  const sortedAnswers = Object.values(answers).sort((a, b) =>
+    a.helpfulness > b.helpfulness ? -1 : 0
+  );
+  console.log(`sorted answers: ${JSON.stringify(sortedAnswers)}`);
+  return (
+    <div>
+      {sortedAnswers.map((answer) => {
+        return (
+          <div>
+            <span>{answer.body}</span>
+            <br />
+            by {answer['answerer_name']}
+            {', '}
+            {new Date(answer.date).formatted()} | Helpful?{' '}
+            <a href="#" style={{ color: '#919191' }}>
+              Yes
+            </a>
+            ({answer.helpfulness}) |{' '}
+            <a href="#" style={{ color: '#919191' }}>
+              report
+            </a>
+          </div>
+        );
+      })}
     </div>
   );
 };
