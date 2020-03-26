235914687735761bf43a4bd3d85fbfa90e72f1e5
Joe Yan <chengmu.yan@gmail.com>
2020-03-26 16:39:23 -0400

    Render dummy ProductRating component

--- client/src/components/overview/productInfo/ProductRating.js
+++ client/src/components/overview/productInfo/ProductRating.js
@@ -1,7 +1,10 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 
 const ProductRating = () => (
-  <div data-testid="productRating">ProductRating component</div>
+  <div data-testid="productRating">
+    ***** <a href="">Read all reviews</a>
+  </div>
 );
 
 export default ProductRating;
