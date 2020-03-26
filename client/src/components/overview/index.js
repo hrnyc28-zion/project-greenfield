6a07b6e36aa686af3bff08aeb96341fc43134f27
Joe Yan <chengmu.yan@gmail.com>
2020-03-26 15:44:54 -0400

    Change styles prop to currentStyles in sampleStore and import in Overview component

--- client/src/components/overview/index.js
+++ client/src/components/overview/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ProductInfo from './productInfo';
+import sampleStore from '../../sampleData/sampleStore';
+
+const { selected, currentProduct, styles } = sampleStore;
 
 const Overview = () => (
   <div data-testid="productOverview">
     Overview component
-    <ProductInfo />
+    <ProductInfo
+      selected={selected}
+      currentProduct={currentProduct}
+      currentStyles={styles}
+    />
   </div>
 );
 
