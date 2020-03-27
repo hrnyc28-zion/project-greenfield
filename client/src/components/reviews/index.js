7f8d188aade1f563f1343163ce3df8ef079a37ac
Dan Silberger <dan.silberger@gmail.com>
2020-03-27 14:17:35 -0400

    added state to ReviewWidget top level

--- client/src/components/reviews/index.js
+++ client/src/components/reviews/index.js
@@ -3,19 +3,28 @@ import ReviewList from './ReviewList';
 import RatingsBreakdown from './RatingsBreakdown';
 import ProductBreakdown from './ProductBreakdown';
 
-const ReviewWidget = ({ sampleStore }) => (
-  <div data-testid="reviews">
-    Reviews Components
-    <section>
-      <ReviewList reviewList={sampleStore.reviewData} />
-    </section>
-    <section>
-      <RatingsBreakdown reviewData={sampleStore.reviewData} />
-    </section>
-    <section>
-      <ProductBreakdown productBreakdown={sampleStore.reviewData} />
-    </section>
-  </div>
-);
+class ReviewWidget extends React.Component {
+  constructor({ sampleStore }) {
+    super(sampleStore);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div data-testid="reviews">
+        Reviews Components
+        <section>
+          <ReviewList reviewList={this.sampleStore.reviewData} />
+        </section>
+        <section>
+          <RatingsBreakdown reviewData={this.sampleStore.reviewData} />
+        </section>
+        <section>
+          <ProductBreakdown productBreakdown={this.sampleStore.reviewData} />
+        </section>
+      </div>
+    );
+  }
+}
 
 export default ReviewWidget;
