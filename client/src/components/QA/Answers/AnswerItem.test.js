ecf6ec23a8aa48b22a59d6c5af0f60810badd173
h01m3s <lin426797@gmail.com>
2020-03-28 14:14:49 -0400

    Plug in dummy data for testing

--- client/src/components/QA/Answers/AnswerItem.test.js
+++ client/src/components/QA/Answers/AnswerItem.test.js
@@ -2,8 +2,17 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import AnswerItem from './AnswerItem';
 
-test('renders learn react link', () => {
-  const { getByTestId } = render(<AnswerItem />);
+const testData = {
+  id: 98,
+  body: 'Suede',
+  date: '2019-10-12T00:00:00.000Z',
+  answerer_name: 'n00bgamer',
+  helpfulness: 71,
+  photos: []
+};
+
+test('renders AnswerItem', () => {
+  const { getByTestId } = render(<AnswerItem answer={testData} />);
   const component = getByTestId('answerItem');
   expect(component).toBeInTheDocument();
 });
--- client/src/components/QA/index.test.js
+++ client/src/components/QA/index.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import QA from './index';
 
-test('renders learn react link', () => {
+test('renders main QA component', () => {
   const { getByTestId } = render(<QA />);
   const component = getByTestId('qaTest');
   expect(component).toBeInTheDocument();
