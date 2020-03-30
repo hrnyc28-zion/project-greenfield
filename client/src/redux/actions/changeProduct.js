c45190683c5f5b3992daa8c39f2f954666d5208f
Joe Yan <chengmu.yan@gmail.com>
2020-03-30 12:12:44 -0400

    Update changeProduct to set styles, current style, thumbnail

--- client/src/redux/actions/changeProduct.js
+++ client/src/redux/actions/changeProduct.js
@@ -1,9 +1,19 @@
-import { QA } from '../../api';
+import { Product, QA } from '../../api';
+import setCurrentProduct from './currentProduct';
+import setStyles from './styles';
+import { setStyle, setThumbnail } from './selected';
 import setQuestions from './setQuestions';
 
 // Use API calls to init redux store
-const changeProduct = (id = 13) => {
+const changeProduct = (id) => {
   return async (dispatch) => {
+    const product = await Product.getProduct(id);
+    dispatch(setCurrentProduct(product));
+
+    const { results: styles } = await Product.getStyles(id);
+    dispatch(setStyles(styles));
+    dispatch(setStyle(styles[0]));
+    dispatch(setThumbnail(0));
     const questions = await QA.fetchAllQuestions(id);
     dispatch(setQuestions(questions.error ? [] : questions.results));
   };
