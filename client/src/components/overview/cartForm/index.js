54b4e073ae60305ff38e6b977a3a92f91ec55a13
Joe Yan <chengmu.yan@gmail.com>
2020-03-30 17:35:14 -0400

    sizeselect renders out of stock when no stock for selected style

--- client/src/components/overview/cartForm/index.js
+++ client/src/components/overview/cartForm/index.js
@@ -1,18 +1,38 @@
 import React from 'react';
-import { Formik, Field, useField } from 'formik';
+import { Formik, Field, useField, useFormikContext } from 'formik';
 import { connect } from 'react-redux';
 
-// const SizeSelect = ({ label, ...props}) => {
-//   const [field, meta] = useField(props);
-//   return (
-//     <label>
-//     <input name="size">
-//     </input>
-//     </label>
-//   )
-// }
+const SizeSelect = ({ sizes }) => {
+  return (
+    <>
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+      <label htmlFor="size" />
+      <Field
+        name="size"
+        as="select"
+        className="form-control"
+        disabled={sizes.length === 0}
+      >
+        <option value="" disabled defaultValue hidden>
+          {sizes.length === 0 ? 'out of stock' : 'select size'}
+        </option>
+        {sizes.map((sku) => (
+          <option value={sku}>{sku}</option>
+        ))}
+      </Field>
+    </>
+  );
+};
 
 const CartForm = ({ skus }) => {
+  const skusInStock = {};
+  // eslint-disable-next-line no-restricted-syntax
+  for (const size in skus) {
+    // TODO: adjust this
+    if (skus[size]) skusInStock[size] = skus[size];
+  }
+  console.log(skus);
+  console.log(skusInStock);
   return (
     <div data-testid="cartForm" className="container">
       <Formik
@@ -23,25 +43,15 @@ const CartForm = ({ skus }) => {
           <form onSubmit={formik.handleSubmit}>
             <div className="form-row">
               <div className="col-md-7 form-group">
-                <label htmlFor="size">
-                  <Field name="size" as="select" className="form-control">
-                    <option value="" disabled defaultValue hidden>
-                      select size
-                    </option>
-                    {Object.keys(skus).map((sku) => (
-                      <option value={sku}>{sku}</option>
-                    ))}
-                  </Field>
-                </label>
+                <SizeSelect sizes={Object.keys(skusInStock)} />
               </div>
               <div className="col-md-5 form-group">
-                <label htmlFor="quantity">
-                  <Field name="quantity" as="select" className="form-control">
-                    {[...new Array(skus[formik.values.size])].map((_, idx) => (
-                      <option value={idx + 1}>{idx + 1}</option>
-                    ))}
-                  </Field>
-                </label>
+                <label htmlFor="quantity" />
+                <Field name="quantity" as="select" className="form-control">
+                  {[...new Array(skus[formik.values.size])].map((_, idx) => (
+                    <option value={idx + 1}>{idx + 1}</option>
+                  ))}
+                </Field>
               </div>
             </div>
             <div className="form-row">
