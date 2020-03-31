cae92b079a6b0df0d131b3b5463d8089baa37146
Dan Silberger <56615914+dsilberger@users.noreply.github.com>
2020-03-31 10:39:22 -0400

    Redux integration review (#58)
    * checking for new prs
    
    * Reviews/Breakdowns reducers and actions
    
    * mysterious reducer failure in breakdowns
    
    * Working except that the breakdowns reducer thinks its returning null even though the response is correct
    
    * checking in redux reducers etc
    
    * this commit is basically functional
    
    * Render red sale price and struck orig price when on sale (#63)
    
    * redux integration complete for review widget
    
    Co-authored-by: Joe Yan <chengmu.yan@gmail.com>

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -4,13 +4,17 @@ import currentStylesReducer from './currentStylesReducer';
 import selectedReducer from './selectedReducer';
 import relatedProductReducer from './relatedProductReducer';
 import currentQuestionsReducer from './currentQuestionsReducer';
+import currentReviewsReducer from './currentReviewsReducer';
+import currentBreakdownsReducer from './currentBreakdownsReducer';
 
 const rootReducer = combineReducers({
   selected: selectedReducer,
   currentProduct: currentProductReducer,
   currentStyles: currentStylesReducer,
   relatedProducts: relatedProductReducer,
-  currentQuestion: currentQuestionsReducer
+  currentQuestion: currentQuestionsReducer,
+  currentReviews: currentReviewsReducer,
+  currentBreakdowns: currentBreakdownsReducer
 });
 
 export default rootReducer;
