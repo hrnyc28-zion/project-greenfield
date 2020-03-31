12642c60f556e276e869176786da6f1dfde3afbd
Vincent Chen <vc88086@gmail.com>
2020-03-31 11:30:21 -0700

    style update

--- client/src/components/related-items/RelateAndCompare.js
+++ client/src/components/related-items/RelateAndCompare.js
@@ -42,7 +42,7 @@ class RelateAndCompare extends React.Component {
       name: currentProduct.name,
       category: currentProduct.category,
       price: currentProduct.default_price,
-      thumbnail_url: selected.style.photos[0].thumbnail_url
+      thumbnail_url: selected.style.photos[0].url
     };
     let storage = localStorage.getItem('yourOutfit');
     if (storage === null) {
